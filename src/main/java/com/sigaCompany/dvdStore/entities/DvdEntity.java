6fb28a02967a01691aa35b06de6edc4a038f488e
guarneri anthony <guarneriAnthony@outlook.fr>
2023-09-14 21:35:20 +0200

    feat : Create the mapping for create one Dvd in the bdd

--- src/main/java/com/sigaCompany/dvdStore/entities/DvdEntity.java
+++ src/main/java/com/sigaCompany/dvdStore/entities/DvdEntity.java
@@ -1,4 +1,32 @@
 package com.sigaCompany.dvdStore.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Entity
+@Data
+@AllArgsConstructor
+@Table(name = "Dvd")
 public class DvdEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column
+    private String name;
+    @Column
+    private String gender;
+    @Column
+    private int quantity;
+    @Column
+    private int price;
+
+    protected DvdEntity(){}
+    public DvdEntity(String name, String gender, int quantity, int price) {
+        this.name = name;
+        this.gender = gender;
+        this.quantity = quantity;
+        this.price = price;
+    }
 }
--- src/main/java/com/sigaCompany/dvdStore/repositories/DvdRepository.java
+++ src/main/java/com/sigaCompany/dvdStore/repositories/DvdRepository.java
@@ -4,4 +4,5 @@ import com.sigaCompany.dvdStore.entities.DvdEntity;
 import org.springframework.data.repository.CrudRepository;
 
 public interface DvdRepository extends CrudRepository<DvdEntity, Long> {
+    DvdEntity save(DvdEntity dvdEntity);
 }
