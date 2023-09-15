f6fa6c692b185ee3b4d2360ef829c93b70b109aa
guarneri anthony <guarneriAnthony@outlook.fr>
2023-09-15 15:09:10 +0200

    feat: Create the Crud for Dvds

--- src/main/java/com/sigaCompany/dvdStore/repositories/DvdRepository.java
+++ src/main/java/com/sigaCompany/dvdStore/repositories/DvdRepository.java
@@ -3,6 +3,9 @@ package com.sigaCompany.dvdStore.repositories;
 import com.sigaCompany.dvdStore.entities.DvdEntity;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface DvdRepository extends CrudRepository<DvdEntity, Long> {
     DvdEntity save(DvdEntity dvdEntity);
+    List<DvdEntity> findAll();
 }
