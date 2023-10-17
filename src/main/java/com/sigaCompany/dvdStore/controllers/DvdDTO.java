18a50484bb9ca7210780b733b4009e4c0ff359c3
guarneri anthony <guarneriAnthony@outlook.fr>
2023-10-17 10:53:34 +0200

    Fix: change local depot

--- src/main/java/com/sigaCompany/dvdStore/controllers/DvdDTO.java
+++ src/main/java/com/sigaCompany/dvdStore/controllers/DvdDTO.java
@@ -2,5 +2,8 @@ package com.sigaCompany.dvdStore.controllers;
 
 import java.util.Optional;
 
+/**
+ * The type Dvd dto.
+ */
 public record DvdDTO(Long id, String name, String type, int quantity, int price, String description, String image) {
 }
--- src/main/java/com/sigaCompany/dvdStore/controllers/SellDTO.java
+++ src/main/java/com/sigaCompany/dvdStore/controllers/SellDTO.java
@@ -1,4 +1,7 @@
 package com.sigaCompany.dvdStore.controllers;
 
+/**
+ * The type Sell dto.
+ */
 public record SellDTO(long id, long clientId, long dvdId, int quantity) {
 }
